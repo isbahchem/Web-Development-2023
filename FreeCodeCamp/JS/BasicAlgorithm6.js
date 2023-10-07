4f2433fb4eb9515315b7915a94ecf5854f618656
isbahchem <isbahmedferoz@gmail.com>
2023-10-07 19:58:23 +0500

    String Repeat BasicAlgorithm6 added

---
+++ FreeCodeCamp/JS/BasicAlgorithm6.js
@@ -0,0 +1,12 @@
+// Repeat a String Repeat a String
+// Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+
+function repeatStringNumTimes(str, num) {
+    let strepeat="";
+    for(let i=0; i<num; i++){
+      strepeat = strepeat+str;
+    }
+    return strepeat;
+  }
+  
+  repeatStringNumTimes("abc", 3);

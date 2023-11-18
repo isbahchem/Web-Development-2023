5e9ff9ff9d6dd9b15f81ade6bc703a367c1b1ca1
isbahchem <isbahmedferoz@gmail.com>
2023-11-18 19:44:55 +0500

    BasicAlgorithm12 Falsy Bouncer added

---
+++ FreeCodeCamp/JS/BasicAlgorithm12.js
@@ -0,0 +1,11 @@
+// Falsy Bouncer
+// Remove all falsy values from an array. Return a new array; do not mutate the original array.
+
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+// Hint: Try converting each value to a Boolean.
+
+function bouncer(arr) {
+    return arr.filter(Boolean);
+  }
+  bouncer([7, "ate", "", false, 9]);
